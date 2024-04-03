a7b8b2d1c746023935136c34c74e8820dc7a0ca0
DTNevaeha <DTNevaeha@gmail.com>
2024-04-03 16:48:12 -0500

    Pendulum scale scenario

---
+++ pendulum_scale.py
@@ -0,0 +1,18 @@
+# You have 12 balls how do you find the heaviest using a pendulum scale?
+
+# A pendulum scale is a type of balance scale that consists of a horizontal beam supported by a central pivot. The beam has
+# two pans or trays suspended from it, one on each side, where objects can be placed for weighing. The scale operates on
+# the principle of torque, where the weight of an object on one side of the beam creates a torque that is balanced by an
+# equal torque on the other side.
+
+# To find the heaviest ball using a pendulum scale, you can follow these steps:
+
+# 12 balls -> 3 groups of 4 balls each
+# Weigh 2 groups against each other
+# If one group is heavier, the heaviest ball is in that group
+# If the groups weigh the same, the heaviest ball is in the third group
+# Divide the group with the heaviest ball into 3 groups of 1 ball each and 1 group of 1 ball
+# Weigh 2 groups against each other
+# If one group is heavier, that ball is the heaviest
+# If they weigh the same, the heaviest ball is the one that was not weighed
+# If the heaviest ball is not found, repeat the process with the remaining group of 4 balls
