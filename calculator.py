cad465ac4b466876645d7371aa2a5e868cf08c58
DTNevaeha <DTNevaeha@gmail.com>
2024-05-20 15:27:21 -0500

    Simplified calculator

--- calculator.py
+++ calculator.py
@@ -4,68 +4,19 @@ print("Hello, please select what you would like to do below."
      "\n 1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide \n Q - to quit")
 
 
-class Addition:
-    def add():
+class MathTime:
+    def math(equation):
         try: 
             x = float(input("Enter the first number: "))
             y = float(input("Enter the second number: "))
         except ValueError:
             print("Please enter a valid number \n"
                   " 1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide \n Q - to quit")
-            # continue
         else:
-            z = float(x) + float(y)
-            print(f"{x} + {y} = {z}")
+            z = eval(f"{x} {equation} {y}")
+            print(f"{x} {equation} {y} = {z}")
             print("\n What would you like to do next?"
-              "\n 1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide \n Q - to quit")
-
-
-class Subtration:
-    def subtract():
-        try: 
-            x = float(input("Enter the first number: "))
-            y = float(input("Enter the second number: "))
-        except ValueError:
-            print("Please enter a valid number \n"
-                    " 1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide \n Q - to quit")
-            # continue
-        else:
-            z = float(x) - float(y)
-            print(f"{x} - {y} = {z}")
-            print("\n What would you like to do next?"
-                "\n 1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide \n Q - to quit")
-
-
-class Multiplication:
-    def multiply():
-        try: 
-            x = float(input("Enter the first number: "))
-            y = float(input("Enter the second number: "))
-        except ValueError:
-            print("Please enter a valid number \n"
-                    " 1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide \n Q - to quit")
-            # continue
-        else:
-            z = float(x) * float(y)
-            print(f"{x} * {y} = {z}")
-            print("\n What would you like to do next?"
-                "\n 1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide \n Q - to quit")
-
-
-class Division:
-    def divide():
-        try: 
-            x = float(input("Enter the first number: "))
-            y = float(input("Enter the second number: "))
-        except ValueError:
-            print("Please enter a valid number \n"
-                    " 1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide \n Q - to quit")
-            # continue
-        else:
-            z = float(x) / float(y)
-            print(f"{x} / {y} = {z}")
-            print("\n What would you like to do next?"
-                "\n 1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide \n Q - to quit")
+            "\n 1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide \n Q - to quit")
 
 
 while True:
@@ -73,17 +24,20 @@ while True:
     if selection == 'q':
         print("Thanks!")
         break
-    elif selection == '1':
-        print("You chose 1")
-        Addition.add()
-    elif selection == '2':
-        print("You chose 2")
-        Subtration.subtract()
-    elif selection == '3':
-        Multiplication.multiply()
-    elif selection == '4':
-        print("You chose 4")
-        Division.divide()
+    elif selection in ['1', '2', '3', '4']:
+        if selection == '1':
+            selection = "+"
+            print("You chose Addition")
+        elif selection == '2':
+            selection = "-"
+            print("You chose Subtraction")
+        elif selection == '3':
+            selection = "*"
+            print("You chose Multiplication")
+        else:
+            selection = "/"
+            print("You chose Division")
+        MathTime.math(selection)
     else:
-        print("That is not a valid input")
-
+        print("That is not a valid input \n"
+              " 1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide \n Q - to quit")
