de0bdbad82cf5c425fa71edc282f58fdb583f8cc
DTNevaeha <DTNevaeha@gmail.com>
2024-05-23 21:30:52 -0500

    Day 1 Python Challenge

---
+++ 100_day_python_challenge/Day_1_band_name.py
@@ -0,0 +1,6 @@
+# What is your band name?
+print("To figure out the name of your band: \n")
+name = input("Enter an animal: ")
+shirt = input("Enter the color of your shirt: ")
+
+print(f"Your band name is The {shirt} {name}s!")
